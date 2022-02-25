43c19ffd4ea8b18179086e5425201afeeb93185e
Nuxnux * <Nuxnux02290@gmail.com>
2022-02-25 19:25:12 +0100

    td2 clecle

---
+++ clecle/td1sln/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Mystere(double a, double b,out double c,out double d)
+        {
+            c = a + b; d = a * b;
+        }
+        public static void Main(string[] args)
+        {
+            double x = 2, y = 3, z, t;
+            Mystere(x, y, out z,out t);
+            Console.WriteLine($"z vaut {z} et t vaut {t}");
+        }
+    }
+}
