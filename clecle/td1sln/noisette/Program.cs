44b3b7488c0954476905ff5dc5703683e65e948d
Nuxnuxx <nuxnux02290@gmail.com>
2022-02-27 19:50:59 +0100

    make program to net 6

---
+++ clecle/td1sln/noisette/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace noisette
+{
+    internal class Program
+    {
+        public static void MAJStockNoisettes(ref int nbNoisettesStock, int nbNoisettesRecoltees)
+        {
+            nbNoisettesStock += nbNoisettesRecoltees;
+        }
+
+        static void Main(string[] args)
+        {
+            int nbStock = Convert.ToInt32(Console.ReadLine());
+            int nbRecolte = Convert.ToInt32(Console.ReadLine());
+            MAJStockNoisettes(ref nbStock, nbRecolte);
+            Console.WriteLine(nbStock);
+        }
+    }
+}
---
+++ clecle/td1sln/piratePartage/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace piratePartage
+{
+    internal class Program
+    {
+
+        public static bool ButinPartage(int pieceOr, int bicorne,int tricorne)
+        {
+            if ((pieceOr - (bicorne * 2 + tricorne * 3)) > 0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            int pieceOr = Convert.ToInt32(Console.ReadLine());
+            int bicorne = Convert.ToInt32(Console.ReadLine());
+            int tricorne = Convert.ToInt32(Console.ReadLine());
+
+            bool statutButin = ButinPartage(pieceOr, bicorne,tricorne);
+            if (statutButin)
+            {
+                Console.WriteLine("Partage possible");
+            } else
+            {
+                Console.WriteLine("Partage impossible");
+            }
+
+        }
+    }
+}
